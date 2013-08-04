dd110ca8fd73871bb076d995d8024112a3aab7b0
JulienBernard <pro.julienbernard@free.fr>
2013-08-04 15:19:05 +0200

    Intégration du code : Structures (bâtiments)
    - Ajout des constantes lié aux bâtiments
    - Ajout de calcul sur le temps
    - Ajout des Getters
    - La page "Structures" est désormais généré automatiquement

--- Pilotage/Models/building.class.php
+++ Pilotage/Models/building.class.php
@@ -11,10 +11,16 @@ class Building
 	private $_costMultiplier;
 	private $_level;
 	private $_population;
+	private $_maxPopulation;
+	private $_superfie;
 	private $_cost1;
 	private $_cost2;
 	private $_cost3;
 	private $_time;
+	
+	/* Constantes */
+	private $_garnisonPerLevel = 20;
+	private $_buildingSuperficie = 100;
 
 	/* Constructeur de la classe */
 	public function __construct( $buildingId, $planetId ) {
@@ -22,26 +28,30 @@ class Building
 	
 		$this->_id = (int)$dataFromDb['bl_id'];
 		$this->_planetId = (int)$dataFromDb['planetId'];
-		$this->_name = (string)$dataFromDb['bl_name'];
-		$this->_description = (string)$dataFromDb['bl_description'];
+		$this->_name = (String)$dataFromDb['bl_name'];
+		$this->_description = (String)$dataFromDb['bl_description'];
 		$this->_type = (int)$dataFromDb['bl_buildingType'];
-		$this->_picture = (string)$dataFromDb['bl_picture'];
+		$this->_picture = (String)$dataFromDb['bl_picture'];
 		$this->_costMultiplier = (double)$dataFromDb['bl_costMultiplier'];
 		$this->_level = (int)$dataFromDb['buildingLevel'];
 		$this->_population = (int)$dataFromDb['buildingPopulation'];
+		$this->_maxPopulation = (int)$this->_level * (int)$this->_garnisonPerLevel;
+		$this->_superfie = (int)$this->_level * (int)$this->_buildingSuperficie;
+
 		if( $this->_level == 0 )
 		{
 			$this->_cost1 = (int)$dataFromDb['bl_cost1'];
 			$this->_cost2 = (int)$dataFromDb['bl_cost2'];
 			$this->_cost3 = (int)$dataFromDb['bl_cost3'];
+			$this->_time = (int)$dataFromDb['bl_buildingTime'] * (double)$this->_costMultiplier;
 		}
 		else
 		{
 			$this->_cost1 = (int)$dataFromDb['bl_cost1'] * (int)$this->_level * (double)$this->_costMultiplier;
 			$this->_cost2 = (int)$dataFromDb['bl_cost2'] * (int)$this->_level * (double)$this->_costMultiplier;
 			$this->_cost3 = (int)$dataFromDb['bl_cost3'] * (int)$this->_level * (double)$this->_costMultiplier;
+			$this->_time = (int)$this->_level * (int)$dataFromDb['bl_buildingTime'] * (double)$this->_costMultiplier;
 		}
-		$this->_time = (int)$this->_level * (int)$dataFromDb['bl_buildingTime'] * (double)$this->_costMultiplier;
 	}
 	
 	/**
@@ -60,5 +70,72 @@ class Building
 		if( $rq->rowCount() == 0 ) throw new Exception('Une importante erreur est survenue : Impossible de récupérer les données de cette structure !');
 		$row = $rq->fetch();
 		return $row;
+	}
+	
+	public function timeToString( $seconds )
+	{
+		$time = "";
+		$days = floor( $seconds / (60*60*24) );
+		$seconds = $seconds % (60*60*24);
+		
+		$hours = floor( $seconds / (60*60) );
+		$seconds = $seconds % (60*60);
+		
+		$minutes = floor( $seconds / (60) );
+		$seconds = $seconds % (60);
+		
+		if( $days > 0 )
+		{
+			$time = $days . ' jour';
+			if( $days > 1 )
+				$time .= 's';
+			
+			$time .= ' ';
+		}
+
+		$time .= sprintf('%02d', $hours) . ':' . sprintf('%02d', $minutes) . ':' . sprintf('%02d', $seconds);
+		
+		return $time;
+	}
+	
+	/* Getters */
+	public function getId() {
+		return $this->_id;
+	}
+	public function getPlanetId() {
+		return $this->_planetId;
+	}
+	public function getName() {
+		return $this->_name;
+	}
+	public function getDescription() {
+		return $this->_description;
+	}
+	public function getType() {
+		return $this->_type;
+	}
+	public function getPicture() {
+		return $this->_picture;
+	}
+	public function getSuperficie() {
+		return $this->_superfie;
+	}
+	public function getPopulation() {
+		return $this->_population;
+	}
+	public function getMaxPopulation() {
+		return $this->_maxPopulation;
+	}
+	public function getCost1() {
+		return $this->_cost1;
+	}
+	public function getCost2() {
+		return $this->_cost2;
+	}
+	public function getCost3() {
+		return $this->_cost3;
+	}
+	public function getTime() {
+		return $this->_time;
 	}
 }
--- Pilotage/Views/structure.connect.php
+++ Pilotage/Views/structure.connect.php
@@ -6,197 +6,37 @@
 			<article class="large-9 columns">
 				<h1>STRUCTURES</h1>
 				
-				<ul class="large-ul">
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>10.000 m²<br />
-						<span class="smaller">
-							<span class="bad">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							&nbsp;<br />
-							<strong>Capitale de l'Empire</strong><br />
-							Vitrine de votre nation, la superficie de votre capitale détermine le taux de natalité de votre empire.
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>100 m²<br />
-						<span class="smaller">
-							<span class="good">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							&nbsp;<br />
-							<strong>Bureau des Régions</strong><br />
-							Le contrôle de l'extension de votre territoire vous permet de réguler votre taux de natalité.
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-				</ul>
-				<ul class="large-ul">
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>300 m²<br />
-						<span class="smaller">
-							<span class="good">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							<br />
-							<strong>Mine de Titane</strong><br />
-							Matière première de votre planète, le titane doit être maintenu à un taux de rendement elevé.
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>200 m²<br />
-						<span class="smaller">
-							<span class="good">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							<br />
-							<strong>Mine de Béryl</strong><br />
-							Le béryl est un matériau proche du cristal utilisé fréquemment dans les technologies de pointes.
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>100 m²<br />
-						<span class="smaller">
-							<span class="good">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							<br />
-							<strong>Station d'extraction d'Hydrogène</strong><br />
-							Carburant écologique mais difficile à extraire, l'hydrogène doit être condensé avant son utilisation.
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>100 m²<br />
-						<span class="smaller">
-							<span class="good">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							<br />
-							<strong>Entrepôts de Titane</strong><br />
-							Entrepôts de stockage de Titane, vous ne pouvez excéder sa capacité.<br />
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>100 m²<br />
-						<span class="smaller">
-							<span class="good">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							<br />
-							<strong>Entrepôts de Béryl</strong><br />
-							 Entrepôts de stockage du Béryl, vous ne pouvez excéder sa capacité.<br />
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>100 m²<br />
-						<span class="smaller">
-							<span class="good">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							<br />
-							<strong>Silos à Hydrogène</strong><br />
-							Silos de stockage d'Hydrogène, vous ne pouvez excéder sa capacité.<br />
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-				</ul>
-				<ul class="large-ul">
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>100 m²<br />
-						<span class="smaller">
-							<span class="good">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							&nbsp;<br />
-							<strong>Centre de Recherche</strong><br />
-							La recherche de nouvelles technologies est essentielle pour l'évolution de votre empire.
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>100 m²<br />
-						<span class="smaller">
-							<span class="good">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							&nbsp;<br />
-							<strong>Usine d'Assemblage</strong><br />
-							C'est à l'usine que vous pourrez imaginer les plans de vos vaisseaux pour ensuite les assembler.
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-					<li>
-						<span class="float-left"><img src="./img/bat/aucun.png" alt="[IMAGE]" /></span>100 m²<br />
-						<span class="smaller">
-							<span class="good">999.999 Titane</span><br />
-							<span class="good">99.999 Béryl</span><br />
-							<span class="good">99.999 Hydro.</span><br />
-							&nbsp;<br />
-							<strong>Atelier de Production</strong><br />
-							L'atelier vous permet de fabriquer différents types de modules, vous permettant d'assembler vos vaisseaux.
-							<br />
-						</span>
-						<form action="<?php echo $namePage; ?>.connect.php" method="POST">
-							<div class="small-12 columns">
-								<input type="submit" value="Agrandir (00:02:42)" class="button prefix"/>
-							</div>
-						</form>
-					</li>
-				</ul>
+				<?php
+					for( $i = 1 ; $i <= 4 ; $i++ )
+					{	?>
+						<ul class="large-ul">
+						<?php
+						$buildings = $Data->getBuildingsList( $i );
+						for( $j = 0 ; $j < count($buildings) ; $j++ )
+						{
+							$canBuy = true;
+						?>
+							<li>
+								<span class="float-left"><img src="./img/bat/<?php echo (String)$buildings[$j]->getPicture(); ?>" alt="[IMAGE]" /></span><?php echo (int)$buildings[$j]->getSuperficie(); ?> m²<br />
+								<span class="smaller">
+									<span class="<?php if( (int)$Data->getRes1() >= (int)$buildings[$j]->getCost1() ) echo 'good'; else { echo 'bad'; $canBuy = false; } ?>"><?php echo (int)$buildings[$j]->getCost1(); ?> Titane</span><br />
+									<span class="<?php if( (int)$Data->getRes2() >= (int)$buildings[$j]->getCost2() ) echo 'good'; else { echo 'bad'; $canBuy = false; } ?>"><?php echo (int)$buildings[$j]->getCost2(); ?> Béryl</span><br />
+									<span class="<?php if( (int)$Data->getRes3() >= (int)$buildings[$j]->getCost3() ) echo 'good'; else { echo 'bad'; $canBuy = false; } ?>"><?php echo (int)$buildings[$j]->getCost3(); ?> Hydro.</span><br />
+									&nbsp;<br />
+									<span class="bold"><?php echo (String)$buildings[$j]->getName(); ?></span><br />
+									<?php echo (String)$buildings[$j]->getDescription(); ?>
+									<br />
+								</span>
+								<form action="<?php echo $namePage; ?>.connect.php" method="POST">
+									<div class="small-12 columns">
+										<input type="hidden" name="extendBuilding" value="<?php echo (int)$buildings[$j]->getId(); ?>" />
+										<input type="submit" <?php if( !$canBuy ) echo 'disabled="disabled"'; ?> value="Agrandir (<?php echo (String)$buildings[$j]->timeToString($buildings[$j]->getTime()); ?>)" class="button prefix"/>
+									</div>
+								</form>
+							</li>
+						<?php
+						}
+						?>
+						</ul>
+			<?php	}	?>
 			</article>
