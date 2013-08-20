d453d6f96ff21602faec6b5a6b5816f0fa82641f
JulienBernard <pro.julienbernard@free.fr>
2013-08-20 18:27:54 +0200

    Création de la classe "Technology"

--- Pilotage/Views/recherche.connect.php
+++ Pilotage/Views/recherche.connect.php
@@ -15,6 +15,44 @@
 				</div>
 
 				<section class="ac-container">
+				<?php
+				for( $i = 1 ; $i <= 4 ; $i++ )
+				{	?>
+					<ul class="large-ul">
+					<?php
+					$buildings = $Data->getBuildingsList( $i );
+					for( $j = 0 ; $j < count($buildings) ; $j++ )
+					{
+						$canBuy = true;
+					?>
+						<li>
+							<span class="float-left"><img src="./img/bat/<?php echo (String)$buildings[$j]->getPicture(); ?>" alt="[IMAGE]" /></span><?php echo (int)$buildings[$j]->getSuperficie(); ?> m²<br />
+							<span class="smaller">
+								<span class="<?php if( (int)$Data->getRes1() >= (int)$buildings[$j]->getCost1() ) echo 'good'; else { echo 'bad'; $canBuy = false; } ?>"><?php echo (int)$buildings[$j]->getCost1(); ?> Titane</span><br />
+								<span class="<?php if( (int)$Data->getRes2() >= (int)$buildings[$j]->getCost2() ) echo 'good'; else { echo 'bad'; $canBuy = false; } ?>"><?php echo (int)$buildings[$j]->getCost2(); ?> Béryl</span><br />
+								<span class="<?php if( (int)$Data->getRes3() >= (int)$buildings[$j]->getCost3() ) echo 'good'; else { echo 'bad'; $canBuy = false; } ?>"><?php echo (int)$buildings[$j]->getCost3(); ?> Hydro.</span><br />
+								&nbsp;<br />
+								<span class="bold"><?php echo (String)$buildings[$j]->getName(); ?></span><br />
+								<?php echo (String)$buildings[$j]->getDescription(); ?>
+								<br />
+							</span>
+							<form action="<?php echo $namePage; ?>.connect.php" method="POST">
+								<div class="small-12 columns">
+									<input type="hidden" name="extendBuilding" value="<?php echo (int)$buildings[$j]->getId(); ?>" />
+									<input type="submit" <?php if( !$canBuy ) echo 'disabled="disabled"'; ?> value="Agrandir (<?php echo (String)$buildings[$j]->timeToString($buildings[$j]->getTime()); ?>)" class="button prefix" />
+								</div>
+							</form>
+						</li>
+					<?php
+					}
+					?>
+					</ul>
+			<?php
+				}
+			?>
+				</section>
+				
+				<section class="ac-container">
 					<div>
 						<input id="ac-1" name="accordion-1" type="radio" />
 						<label for="ac-1">Énergie</label>
