609b12a0af130e7e57ecd8166ba5d4c822d904e6
JulienBernard <pro.julienbernard@free.fr>
2013-08-26 00:53:13 +0200

    Ajout des fichiers de créations SQL.

---
+++ Pilotage/create.sql
@@ -0,0 +1,417 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client: localhost
+-- Généré le: Dim 25 Août 2013 à 22:49
+-- Version du serveur: 5.5.24-log
+-- Version de PHP: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `apocalyspace`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `btop`
+--
+
+CREATE TABLE IF NOT EXISTS `btop` (
+  `buildingId` int(11) NOT NULL,
+  `planetId` int(11) NOT NULL,
+  `buildingLevel` int(11) DEFAULT '0',
+  `buildingPopulation` int(11) DEFAULT '0',
+  PRIMARY KEY (`buildingId`,`planetId`),
+  KEY `planetId` (`planetId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `buildings`
+--
+
+CREATE TABLE IF NOT EXISTS `buildings` (
+  `bl_id` int(11) NOT NULL AUTO_INCREMENT,
+  `bl_name` varchar(40) DEFAULT NULL,
+  `bl_description` text,
+  `bl_buildingType` int(11) DEFAULT NULL,
+  `bl_picture` varchar(64) DEFAULT NULL,
+  `bl_cost1` int(11) DEFAULT NULL,
+  `bl_cost2` int(11) DEFAULT NULL,
+  `bl_cost3` int(11) DEFAULT NULL,
+  `bl_buildingTime` int(11) DEFAULT '600',
+  `bl_costMultiplier` float(3,2) DEFAULT '2.00',
+  PRIMARY KEY (`bl_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `communications`
+--
+
+CREATE TABLE IF NOT EXISTS `communications` (
+  `com_id` int(11) NOT NULL AUTO_INCREMENT,
+  `com_senderId` int(11) NOT NULL,
+  `com_recipientId` int(11) NOT NULL,
+  `com_subject` varchar(255) DEFAULT NULL,
+  `com_message` text,
+  `com_sendTime` int(11) NOT NULL,
+  `com_view` int(11) NOT NULL DEFAULT '0' COMMENT '1: lu - 0: non lu',
+  PRIMARY KEY (`com_id`),
+  KEY `com_senderId` (`com_senderId`),
+  KEY `com_recipientId` (`com_recipientId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `defenseplans`
+--
+
+CREATE TABLE IF NOT EXISTS `defenseplans` (
+  `dpl_id` int(11) NOT NULL AUTO_INCREMENT,
+  `dpl_name` varchar(60) DEFAULT NULL,
+  `dpl_userId` int(11) DEFAULT NULL,
+  `dpl_modules` varchar(255) DEFAULT NULL,
+  `dpl_cost1` int(11) DEFAULT NULL,
+  `dpl_cost2` int(11) DEFAULT NULL,
+  `dpl_cost3` int(11) DEFAULT NULL,
+  `dpl_baseTime` int(11) DEFAULT NULL,
+  PRIMARY KEY (`dpl_id`),
+  KEY `dpl_userId` (`dpl_userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `defenses`
+--
+
+CREATE TABLE IF NOT EXISTS `defenses` (
+  `def_id` int(11) NOT NULL DEFAULT '0',
+  `def_planetId` int(11) NOT NULL DEFAULT '0',
+  `def_quantity` int(11) DEFAULT '0',
+  PRIMARY KEY (`def_id`,`def_planetId`),
+  KEY `def_planetId` (`def_planetId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fleets`
+--
+
+CREATE TABLE IF NOT EXISTS `fleets` (
+  `fl_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fl_userId` int(11) NOT NULL,
+  `fl_type` int(11) DEFAULT NULL,
+  `fl_originPlanetId` int(11) DEFAULT NULL,
+  `fl_destinationPlanetId` int(11) DEFAULT NULL,
+  `fl_departingTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `fl_arrivalTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `fl_resource1` int(11) DEFAULT '0',
+  `fl_resource2` int(11) DEFAULT '0',
+  `fl_resource3` int(11) DEFAULT '0',
+  PRIMARY KEY (`fl_id`),
+  KEY `fl_originPlanetId` (`fl_originPlanetId`),
+  KEY `fl_destinationPlanetId` (`fl_destinationPlanetId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ftos`
+--
+
+CREATE TABLE IF NOT EXISTS `ftos` (
+  `fleetId` int(11) NOT NULL DEFAULT '0',
+  `shipId` int(11) NOT NULL DEFAULT '0',
+  `fleetQuantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`fleetId`,`shipId`),
+  KEY `shipId` (`shipId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `modules`
+--
+
+CREATE TABLE IF NOT EXISTS `modules` (
+  `mod_id` int(11) NOT NULL AUTO_INCREMENT,
+  `mod_name` varchar(40) DEFAULT NULL,
+  `mod_bonusType` int(11) DEFAULT NULL,
+  `mod_bonusValue` varchar(100) DEFAULT NULL,
+  `mod_baseTime` int(11) DEFAULT NULL,
+  `mod_cost1` int(11) DEFAULT NULL,
+  `mod_cost2` int(11) DEFAULT NULL,
+  `mod_cost3` int(11) DEFAULT NULL,
+  PRIMARY KEY (`mod_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mtou`
+--
+
+CREATE TABLE IF NOT EXISTS `mtou` (
+  `moduleId` int(11) NOT NULL DEFAULT '0',
+  `userId` int(11) NOT NULL DEFAULT '0',
+  `moduleType` int(11) DEFAULT NULL,
+  `moduleView` int(11) DEFAULT '0',
+  PRIMARY KEY (`moduleId`,`userId`),
+  KEY `userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ongoingbuilds`
+--
+
+CREATE TABLE IF NOT EXISTS `ongoingbuilds` (
+  `gb_id` int(11) NOT NULL AUTO_INCREMENT,
+  `gb_buildType` int(11) NOT NULL,
+  `gb_buildId` int(11) NOT NULL,
+  `gb_buildQuantity` int(11) NOT NULL,
+  `gb_planetId` int(11) DEFAULT NULL,
+  `gb_userId` int(11) DEFAULT NULL,
+  `gb_startTime` int(11) NOT NULL,
+  `gb_endTime` int(11) NOT NULL,
+  PRIMARY KEY (`gb_id`),
+  KEY `gb_userId` (`gb_userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `planets`
+--
+
+CREATE TABLE IF NOT EXISTS `planets` (
+  `pl_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pl_name` varchar(40) NOT NULL,
+  `pl_planetSize` int(11) DEFAULT NULL,
+  `pl_population` int(11) DEFAULT NULL,
+  `pl_userId` int(11) NOT NULL,
+  `pl_posX` int(11) DEFAULT NULL,
+  `pl_posY` int(11) DEFAULT NULL,
+  `pl_res1` int(11) DEFAULT NULL,
+  `pl_res2` int(11) DEFAULT NULL,
+  `pl_res3` int(11) DEFAULT NULL,
+  `pl_pr` int(11) NOT NULL,
+  `pl_prod_res1` int(11) NOT NULL DEFAULT '0',
+  `pl_prod_res2` int(11) NOT NULL DEFAULT '0',
+  `pl_prod_res3` int(11) NOT NULL DEFAULT '0',
+  `pl_prod_pr` int(11) NOT NULL,
+  `pl_prod_time` int(11) NOT NULL,
+  `pl_natality` int(11) NOT NULL,
+  `pl_primary` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`pl_id`),
+  UNIQUE KEY `pl_id` (`pl_id`),
+  UNIQUE KEY `uc_Position` (`pl_posX`,`pl_posY`),
+  KEY `pl_userId` (`pl_userId`),
+  KEY `pl_id_2` (`pl_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `shipplans`
+--
+
+CREATE TABLE IF NOT EXISTS `shipplans` (
+  `spl_id` int(11) NOT NULL AUTO_INCREMENT,
+  `spl_name` varchar(60) DEFAULT NULL,
+  `spl_userId` int(11) DEFAULT NULL,
+  `spl_modules` varchar(255) DEFAULT NULL,
+  `spl_cost1` int(11) DEFAULT NULL,
+  `spl_cost2` int(11) DEFAULT NULL,
+  `spl_cost3` int(11) DEFAULT NULL,
+  `spl_baseTime` int(11) DEFAULT NULL,
+  PRIMARY KEY (`spl_id`),
+  KEY `spl_userId` (`spl_userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ships`
+--
+
+CREATE TABLE IF NOT EXISTS `ships` (
+  `shp_id` int(11) NOT NULL DEFAULT '0',
+  `shp_planetId` int(11) NOT NULL DEFAULT '0',
+  `shp_quantity` int(11) DEFAULT '0',
+  PRIMARY KEY (`shp_id`,`shp_planetId`),
+  KEY `shp_planetId` (`shp_planetId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stock`
+--
+
+CREATE TABLE IF NOT EXISTS `stock` (
+  `st_moduleId` int(11) NOT NULL DEFAULT '0',
+  `st_planetId` int(11) NOT NULL DEFAULT '0',
+  `st_quantity` int(11) DEFAULT '0',
+  PRIMARY KEY (`st_moduleId`,`st_planetId`),
+  KEY `st_planetId` (`st_planetId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `technologies`
+--
+
+CREATE TABLE IF NOT EXISTS `technologies` (
+  `th_id` int(11) NOT NULL AUTO_INCREMENT,
+  `th_name` varchar(40) DEFAULT NULL,
+  `th_pitch` varchar(255) NOT NULL,
+  `th_description` text,
+  `th_informations` varchar(255) NOT NULL,
+  PRIMARY KEY (`th_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ttou`
+--
+
+CREATE TABLE IF NOT EXISTS `ttou` (
+  `userId` int(11) NOT NULL DEFAULT '0',
+  `techId` int(11) NOT NULL DEFAULT '0',
+  `techLevel` int(11) DEFAULT '0',
+  PRIMARY KEY (`userId`,`techId`),
+  KEY `techId` (`techId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `passwordHash` char(32) NOT NULL,
+  `factionName` varchar(50) NOT NULL DEFAULT 'vagabonds',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `id` (`id`),
+  KEY `id_2` (`id`),
+  KEY `id_3` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `btop`
+--
+ALTER TABLE `btop`
+  ADD CONSTRAINT `btop_ibfk_3` FOREIGN KEY (`buildingId`) REFERENCES `buildings` (`bl_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `btop_ibfk_4` FOREIGN KEY (`planetId`) REFERENCES `planets` (`pl_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `communications`
+--
+ALTER TABLE `communications`
+  ADD CONSTRAINT `communications_ibfk_3` FOREIGN KEY (`com_senderId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `communications_ibfk_4` FOREIGN KEY (`com_recipientId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `defenseplans`
+--
+ALTER TABLE `defenseplans`
+  ADD CONSTRAINT `defenseplans_ibfk_2` FOREIGN KEY (`dpl_userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `defenses`
+--
+ALTER TABLE `defenses`
+  ADD CONSTRAINT `defenses_ibfk_3` FOREIGN KEY (`def_id`) REFERENCES `defenseplans` (`dpl_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `defenses_ibfk_4` FOREIGN KEY (`def_planetId`) REFERENCES `planets` (`pl_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `fleets`
+--
+ALTER TABLE `fleets`
+  ADD CONSTRAINT `fleets_ibfk_3` FOREIGN KEY (`fl_originPlanetId`) REFERENCES `planets` (`pl_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fleets_ibfk_4` FOREIGN KEY (`fl_destinationPlanetId`) REFERENCES `planets` (`pl_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `ftos`
+--
+ALTER TABLE `ftos`
+  ADD CONSTRAINT `ftos_ibfk_3` FOREIGN KEY (`fleetId`) REFERENCES `fleets` (`fl_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ftos_ibfk_4` FOREIGN KEY (`shipId`) REFERENCES `shipplans` (`spl_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `mtou`
+--
+ALTER TABLE `mtou`
+  ADD CONSTRAINT `mtou_ibfk_3` FOREIGN KEY (`moduleId`) REFERENCES `modules` (`mod_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `mtou_ibfk_4` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `ongoingbuilds`
+--
+ALTER TABLE `ongoingbuilds`
+  ADD CONSTRAINT `ongoingbuilds_ibfk_2` FOREIGN KEY (`gb_userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `planets`
+--
+ALTER TABLE `planets`
+  ADD CONSTRAINT `planets_ibfk_2` FOREIGN KEY (`pl_userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `shipplans`
+--
+ALTER TABLE `shipplans`
+  ADD CONSTRAINT `shipplans_ibfk_2` FOREIGN KEY (`spl_userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `ships`
+--
+ALTER TABLE `ships`
+  ADD CONSTRAINT `ships_ibfk_3` FOREIGN KEY (`shp_id`) REFERENCES `shipplans` (`spl_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ships_ibfk_4` FOREIGN KEY (`shp_planetId`) REFERENCES `planets` (`pl_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_3` FOREIGN KEY (`st_moduleId`) REFERENCES `modules` (`mod_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `stock_ibfk_4` FOREIGN KEY (`st_planetId`) REFERENCES `planets` (`pl_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `ttou`
+--
+ALTER TABLE `ttou`
+  ADD CONSTRAINT `ttou_ibfk_3` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ttou_ibfk_4` FOREIGN KEY (`techId`) REFERENCES `technologies` (`th_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Pilotage/insert.sql
@@ -0,0 +1,79 @@
+﻿SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET FOREIGN_KEY_CHECKS=0;
+
+TRUNCATE TABLE `buildings`;
+TRUNCATE TABLE `technologies`;
+TRUNCATE TABLE `modules`;
+
+-- --------------------------------------------------------
+
+INSERT INTO `buildings` (`bl_id`, `bl_name`, `bl_description`, `bl_buildingType`, `bl_picture`, `bl_cost1`, `bl_cost2`, `bl_cost3`, `bl_buildingTime`, `bl_costMultiplier`) VALUES
+(1, 'Capitale de l''Empire', ' Vitrine de votre nation, la superficie de votre capitale détermine le taux de natalité de votre empire. ', 1, 'aucun.png', 550, 380, 0, 150, 2.00),
+(2, ' Bureau des Régions', ' Le contrôle de l''extension de votre territoire vous permet de réguler votre taux de natalité. ', 1, 'habitation.png', 380, 290, 120, 300, 1.75),
+(3, 'Mine de Titane', ' Matière première de votre planète, le titane doit être maintenu à un taux de rendement elevé. ', 2, 'aucun.png', 180, 90, 0, 15, 1.80),
+(4, 'Mine de Béryl', ' Le béryl est un matériau proche du cristal utilisé fréquemment dans les technologies de pointes. ', 2, 'aucun.png', 240, 160, 10, 30, 1.80),
+(5, ' Station d''extraction d''Hydrogène', ' Carburant écologique mais difficile à extraire, l''hydrogène doit être condensé avant son utilisation. ', 2, 'aucun.png', 380, 320, 0, 45, 1.80),
+(6, 'Atelier de production', ' L''atelier vous permet de fabriquer différents types de modules, vous permettant d''assembler vos vaisseaux. ', 4, 'aucun.png', 1500, 750, 1000, 400, 1.70),
+(7, 'Usine d''assemblage', ' C''est à l''usine que vous pourrez imaginer les plans de vos vaisseaux pour ensuite les assembler.\r\n ', 4, 'aucun.png', 1500, 500, 500, 450, 2.00),
+(8, 'Entrepôts de Titane', 'Entrepôts de stockage de Titane, vous ne pouvez excéder sa capacité.\n<br />', 3, 'aucun.png', 1000, 0, 0, 100, 2.00),
+(9, 'Entrepôts de Béryl', 'Entrepôts de stockage du Béryl, vous ne pouvez excéder sa capacité.\r\n<br />', 3, 'aucun.png', 1000, 500, 45, 150, 2.00),
+(10, 'Silos à Hydrogène', 'Silos de stockage d''Hydrogène, vous ne pouvez excéder sa capacité.<br />', 3, 'aucun.png', 1500, 500, 210, 200, 2.00),
+(11, 'Centre de recherche', ' La recherche de nouvelles technologies est essentielle pour l''évolution de votre empire. ', 4, 'aucun.png', 520, 600, 400, 120, 2.25);
+
+
+-- --------------------------------------------------------
+
+-- --------------------------------------------------------
+
+-- --------------------------------------------------------
+
+INSERT INTO `technologies` (`th_id`, `th_name`, `th_pitch`, `th_description`, `th_informations`) VALUES
+(1, 'Énergie', '" Que ferions-nous sans énergie ? "', 'Ressource ultime de toute civilisation, l''amélioration de l''énergie vous permettra d''améliorer et de découvrir de nouvelles technologies.', ''),
+(2, 'Production Minière', 'La technologie au service de l''homme !', 'Améliorer la vitesse de collecte des ressources vous octroi (pour chaque niveau d''amélioration) 5 % de production en plus sur toutes les ressources (or PR).', ''),
+(3, 'Recherche Médicale', 'Pour une santé de fer !', 'La médecine permet à votre peuple de vivre dans de meilleures conditions. Chaque niveau d''amélioration augmente votre taux de natalité de 5 %.', 'Attention, ce bonus n''est plus pris en compte en cas de surpopulation !'),
+(4, 'Contrôle des flottes', '" Toujours avoir l''avantage numérique "', 'L''évolution permanente de l''informatique dans l''empire vous permet de contrôler plus de flottes depuis votre centre de commande. Un niveau d''amélioration c''est un nouveau slot de flotte disponible !', ''),
+(5, 'Ingénierie', 'Former pour détruire ?', 'La formation de jeunes ingénieurs ajouter à une technologique de pointe permet à tout empire digne de ce nom de posséder de plus gros vaisseau de guerre.', 'L''ingénierie vous permet d''obtenir des châssis de plus en plus évolués.'),
+(6, 'Armement', '" Le nerf de la guerre se trouve ici, mon grand ... "', 'La recherche en armement vous permettra d''avoir l''avantage en puissance sur vos ennemis. Chaque niveau d''amélioration vous octroi 5 % de puissance en plus.', 'Améliorer cette technologie pour obtenir des modules d''armement plus évolués.'),
+(7, 'Bouclier', '" ... mais également ici ! "', 'Qui n''a jamais rêvé d''une sécurité renforcée pour son vaisseau pour un poids minime ? Chaque niveau d''amélioration vous octroi 5 % de bouclier en plus.', 'Améliorer cette technologie pour obtenir des modules de boucliers plus évolués.'),
+(8, 'Blindage', 'Il n''y a que cela qui te protège du vide de l''espace !', 'Le renforcement de la coque de chaque vaisseau est un processus long et coûteux, mais chaque niveau d''amélioration vous octroi 5 % de coque en plus.', 'Améliorer cette technologie pour obtenir de nouveau modules de coques !'),
+(9, 'Propulsion', 'Plus vite que la lumière !', 'Plus un propulseur est puissant, plus votre vaisseau ira vite. Il faut également prendre en compte le poids des modules ce qui agace souvent de nombreux chercheurs !', 'Améliorer cette technologie pour obtenir des modules de propulsion plus évolués.');
+
+-- --------------------------------------------------------
+
+-- --------------------------------------------------------
+
+-- --------------------------------------------------------
+-- Type des modules - 0: Châssis; 1: Propulsion; 2: Armement; 3: Bouclier; 4: Coque; 5: Stockage; 6: Supplémentaire
+INSERT INTO `modules` (`mod_id`, `mod_name`, `mod_bonusType`, `mod_bonusValue`, `mod_baseTime`, `mod_cost1`, `mod_cost2`, `mod_cost3`) VALUES
+(1, 'Châssis #01', 0, '12;2', 120, 1200, 200, 0), -- Chassis
+(2, 'Châssis #02', 0, '15;3', 200, 1500, 300, 0),
+(3, 'Châssis #03', 0, '18;3', 280, 1800, 300, 0),
+(4, 'Châssis #04', 0, '20;4', 360, 2000, 400, 0),
+(5, 'Châssis #05', 0, '25;4', 440, 2500, 400, 0),
+(6, 'Châssis #06', 0, '32;5', 520, 3200, 500, 0),
+(7, 'Châssis #07', 0, '40;6', 600, 4000, 1300, 0),
+(8, 'Propulsion #01', 1, '4;10', 85, 400, 400, 1000), -- Propulsion
+(9, 'Propulsion #02', 1, '6;13', 190, 600, 600, 1300),
+(10, 'Propulsion #03', 1, '10;16', 300, 1000, 1000, 1600),
+(11, 'Propulsion #04', 1, '15;20', 450, 1500, 1500, 2000),
+(12, 'Armement #01', 2, '3;8', 75, 300, 150, 100), -- Armement
+(13, 'Armement #02', 2, '5;10', 140, 500, 250, 175),
+(14, 'Armement #03', 2, '9;14', 260, 900, 450, 300),
+(15, 'Armement #04', 2, '15;19', 400, 1500, 750, 500),
+(16, 'Bouclier #01', 3, '4;6', 120, 400, 800, 200), -- Bouclier
+(17, 'Bouclier #02', 3, '8;9', 200, 800, 1600, 400),
+(18, 'Bouclier #03', 3, '13;14', 300, 1300, 2600, 650),
+(19, 'Coque #01', 4, '8;100', 90, 800, 0, 0), -- Coque
+(20, 'Coque #02', 4, '13;300', 160, 1300, 0, 0),
+(21, 'Stockage #01', 5, '2;3000', 60, 2000, 0, 0), -- Stockage
+(22, 'Stockage #02', 5, '4;6000', 120, 4000, 0, 0),
+(23, 'Stockage #03', 5, '7;12000', 240, 7000, 0, 0),
+(24, 'Stockage #04', 5, '12;24000', 480, 12000, 0, 0),
+(25, 'Camouflage', 6, '4;Camouflage', 750, 400, 1200, 2400), -- Other
+(26, 'Hyperspace', 6, '8;Hyperspace', 600, 800, 800, 3200),
+(27, 'Infanterie Légère', 7, '4;100', 90, 400, 200, 0), -- Ground Forces
+(28, 'Infanterie Lourde', 7, '6;150', 180, 600, 300, 0),
+(29, 'Blindé Léger', 7, '8;225', 60, 800, 400, 200),
+(30, 'Blindé Lourd', 7, '11;350', 60, 1100, 550, 275);
+
+SET FOREIGN_KEY_CHECKS=1;
