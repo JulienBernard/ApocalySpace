295d61ab23c0f2db0162b8f8d135969d3bae8588
JulienBernard <pro.julienbernard@free.fr>
2013-09-05 19:02:51 +0200

    Ajout de la pagination sur la page "Classement" :
    - Amélioration du SpaceEngine (inclusion de la classe Buildings dans toutes les pages)

--- Pilotage/SpaceEngine/engine.class.php
+++ Pilotage/SpaceEngine/engine.class.php
@@ -38,20 +38,17 @@ class Engine implements IEngine {
 			include_once(PATH_MODELS."planet.class.php");
 			include_once(PATH_MODELS."communication.class.php");
 			include_once(PATH_MODELS."data.class.php");
-			
-			/* Chargement des données sur les bâtiments et les technologies : dépend de la page (gain ressource) */
-			if( $this->_namePage == "structure" OR $this->_namePage == "accueil" )
-			{
-				include_once(PATH_MODELS."building.class.php");
-				$Data = new Data( true, false );
-			}
-			else if( $this->_namePage == "recherche" )
+			include_once(PATH_MODELS."building.class.php");
+							include_once(PATH_MODELS."building.class.php");
+
+			/* Chargement des données sur les technologies : dépend de la page (gain temps chargement) */
+			if( $this->_namePage == "recherche" )
 			{
 				include_once(PATH_MODELS."technology.class.php");
 				$Data = new Data( false, true );
 			}
 			else
-				$Data = new Data( false, false );
+				$Data = new Data( true, false );
 		
 			$Engine->setControllerPath('./Controllers/'.strtolower($namePage).'.connect.php');
 			$Engine->setViewPath('./Views/'.strtolower($namePage).'.connect.php');
