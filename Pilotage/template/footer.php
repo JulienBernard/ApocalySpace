a0c73519d2f7cd41c637142957b1e95de5057956
JulienBernard <pro.julienbernard@free.fr>
2013-09-11 20:00:24 +0200

    Changement de version : 1.6.11

--- Pilotage/template/footer.php
+++ Pilotage/template/footer.php
@@ -61,7 +61,7 @@
 				<span class="italic small">Citation de Napoléon Bonaparte</span>
 			</p>
 			<p class="large-4 columns">
-				ApocalySpace © 2012-2013 &nbsp;&nbsp;&nbsp; <a href="./docs/">Version 1.6.10</a>
+				ApocalySpace © 2012-2013 &nbsp;&nbsp;&nbsp; <a href="./docs/">Version 1.6.11</a>
 			</p>
 		</div>
 	</header>
